804b35dabb3102f60866b502cddc97bf22bfdee8
Aditi Jadhav <aditijadhav221103@gmail.com>
2025-08-08 14:17:01 +0530

    Add leader functionality

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ let home_foul = 0
 function home_add_1() {
     home_score += 1
     homescoreEl.textContent = home_score
+    leader()
 }
 
 function home_add_2() {
     home_score += 2
     homescoreEl.textContent = home_score
+    leader()
 }
 
 function home_add_3() {
     home_score += 3
     homescoreEl.textContent = home_score
+    leader()
 }
 
 function home_incrementFoul() {
@@ -33,21 +36,60 @@ let guest_foul = 0
 function guest_add_1() {
     guest_score += 1
     guestscoreEl.textContent = guest_score
+    leader()
 }
 
 function guest_add_2() {
     guest_score += 2
     guestscoreEl.textContent = guest_score
+    leader()
 }
 
 function guest_add_3() {
     guest_score += 3
     guestscoreEl.textContent = guest_score
+    leader()
 }
 
 function guest_incrementFoul() {
     guest_foul += 1
     guestfoulEl.textContent = guest_foul
+}
+
+function leader() {
+    if (home_score > guest_score) {
+        document.getElementById("home").style.color = "rgb(249, 122, 31)";
+        homescoreEl.style.color = "rgb(249, 122, 31)";
+        document.getElementById("home-box").style.border = "5px solid rgb(249, 122, 31)";
+        document.getElementById("home-box").style.boxShadow = "0 0 20px rgb(249, 122, 31)";
+
+        document.getElementById("guest").style.color = "white";
+        guestscoreEl.style.color = "white";
+        document.getElementById("guest-box").style.border = "5px solid rgb(58, 58, 60)"
+        document.getElementById("guest-box").style.boxShadow = "none";
+
+    } else if (guest_score > home_score) {
+        document.getElementById("guest").style.color = "rgb(249, 122, 31)";
+        guestscoreEl.style.color = "rgb(249, 122, 31)";
+        document.getElementById("guest-box").style.border = "5px solid rgb(249, 122, 31)";
+        document.getElementById("guest-box").style.boxShadow = "0 0 20px rgb(249, 122, 31)";
+
+        document.getElementById("home").style.color = "white";
+        homescoreEl.style.color = "white";
+        document.getElementById("home-box").style.border = "5px solid rgb(58, 58, 60)";
+        document.getElementById("home-box").style.boxShadow = "none";
+
+    } else { // tie
+        document.getElementById("home").style.color = "white";
+        homescoreEl.style.color = "white";
+        document.getElementById("home-box").style.border = "5px solid rgb(58, 58, 60)";
+        document.getElementById("home-box").style.boxShadow = "none";
+
+        document.getElementById("guest").style.color = "white";
+        guestscoreEl.style.color = "white";
+        document.getElementById("guest-box").style.border = "5px solid rgb(58, 58, 60)"
+        document.getElementById("guest-box").style.boxShadow = "none";
+    }
 }
 
 function newGame() {
@@ -59,4 +101,5 @@ function newGame() {
     homefoulEl.textContent = home_foul
     guestscoreEl.textContent = guest_score
     guestfoulEl.textContent = guest_foul
+    leader()
 }
